9b355a9e8a4ba76c46d3a7245fdae43eea847576
Leo <leonardodeaguiar@outlook.com.br>
2023-09-24 14:41:51 -0300

    refactor: adicionado comentarios ao algoritmo de fila dinamica

--- 04-fila-dinamica/fila_dinamica.cpp
+++ 04-fila-dinamica/fila_dinamica.cpp
@@ -21,45 +21,46 @@ FilaDinamica::~FilaDinamica()
 }
 void FilaDinamica::inserir(TipoItem item)
 {
-    if (primeiro == NULL)
+    if (primeiro == NULL) // se não existir o primeiro nó
     {
-        No *novoNo = new No();
-        novoNo->valor = item;
-        novoNo->anterior = NULL;
-        novoNo->proximo = NULL;
-        primeiro = novoNo;
-        ultimo = novoNo;
+        No *novoNo = new No();   // cria um novo nó
+        novoNo->valor = item;    // adiciona o valor de item ao novo nó
+        novoNo->anterior = NULL; // diz que o anterior é nulo
+        novoNo->proximo = NULL;  // diz que o proximo é nulo
+        primeiro = novoNo;       // seta o primeiro como o novo nó
+        ultimo = novoNo;         // seta o ultimo também como o novo nó
     }
     else
 
     {
-        No *novoNo = new No();
-        novoNo->valor = item;
-        ultimo->proximo = novoNo;
-        novoNo->anterior = ultimo;
-        ultimo = novoNo;
-        novoNo->proximo = NULL;
+        No *novoNo = new No();     // cria um novo nó
+        novoNo->valor = item;      // adiciona o valor de item ao novo nó
+        ultimo->proximo = novoNo;  // diz que o proximo do ultimo é o novo nó
+        novoNo->anterior = ultimo; // diz que o nó anterior do novo nó é o atual ultimo
+        ultimo = novoNo;           // seta o ultimo nó como o novo nó
+        novoNo->proximo = NULL;    // diz que o proximo nó é nulo afinal este é o ultimo
     }
 }
 TipoItem FilaDinamica::remover()
 {
     TipoItem item;
-    if (estavazio())
+    if (estavazio()) // não achou o novo nó
     {
         cout << "Lista vazia, não foi possivel excluir!";
         return 0;
     }
     else
     {
-        No *NoTemp = primeiro;
-        item = primeiro->valor;
-        primeiro = NoTemp->proximo;
-        delete NoTemp;
-        return item;
+        No *NoTemp = primeiro;      // nó temporario recebe o primeiro
+        item = primeiro->valor;     // o item recebe o valor do primeiro pra exibir
+        primeiro = NoTemp->proximo; // o primeiro recebe o proximo da fila
+        delete NoTemp;              // deleta a referencia do antigo primeiro nó
+        return item;                // retorna o valor do item
     }
     cout << endl;
 }
 
+// retorna bad aloc se a memoria estiver cheia
 bool FilaDinamica::estaCheio()
 {
     No *novoNo;
@@ -75,11 +76,13 @@ bool FilaDinamica::estaCheio()
     }
 }
 
+// se não existir um primeiro retorna true
 bool FilaDinamica::estavazio()
 {
     return (primeiro == NULL);
 }
 
+// imprime os itens da fila com a funcao proximo
 void FilaDinamica::imprimir()
 {
     No *NoTemp = primeiro;
